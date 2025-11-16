e67b54ada4059a53d9edac2fc2b2e10749ecac3f
Fernando <fernando.r.o.pereira@gmail.com>
2025-11-16 21:48:51 +0000

    TDD -  Red phase

--- tests/test_nth_upper.py
+++ tests/test_nth_upper.py
@@ -2,3 +2,22 @@
 Testes para a função nth_upper presente no módulo src/nth_upper.py
 
 """
+
+import sys
+import os
+
+# Adiciona a pasta src ao path para conseguir importar o módulo
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from nth_upper import nth_upper  # importa a função que vamos criar
+
+def test_nth_upper_n1():
+    """
+    Teste TDD: N = 1, deve retornar todas as maiúsculas do texto
+    """
+    text = "ITCLiNicAl"
+    n = 1
+    expected = "ITCLNA"
+    assert nth_upper(text, n) == expected
+
+
